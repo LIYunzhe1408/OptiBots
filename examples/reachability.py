2a7bf06b6d5c24372ee01d37846783ff3051d1ce
Ale-ZJ <alexandrazj28@gmail.com>
2025-03-14 13:35:24 -0700

    fix some bugs

--- examples/reachability.py
+++ examples/reachability.py
@@ -1,21 +1,22 @@
-from multiprocessing import Pool
 import numpy as np
-import timor
-from timor.Module import *
-from timor.utilities.visualization import animation
 import matplotlib.pyplot as plt
 from itertools import product
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 import time
 from pathlib import Path
-import pickle
 import plotly.graph_objects as go # interactive graphs
 from math import ceil
 
-
-import threading
+import timor
 from timor.utilities import prebuilt_robots
+from timor.task.Task import *
+from timor.task.Obstacle import *
+from timor.Module import *
+from timor.utilities.visualization import animation
+from timor.Geometry import Box, ComposedGeometry, Cylinder
+
+
 
 
 class Reachability():    
@@ -37,158 +38,46 @@ class Reachability():
         
         self.ee_downwards_axis = [0,0,-1]
         self.safety_margin = 0.2
+        self.obstacles_found = 0
         
         self.valid_poses = []
         
-        
-    def check_pose_and_get_gripper(self, theta):
-        """Evaluate whether it is a valid pose (contains no collision).
-
-        Args:
-            theta (list[float x n]): joint angles configuration
-
-        Returns:
-            tuple[float, float, float]: the pose of the end effector if valid
-            None: otherwise 
-            
-        Note:
-            TODO: need to include RRT check
-        """
-        robot = self.robot
-        g = self.robot.fk(theta, collision=True)
-        self_collision = self.robot.has_self_collision()
-        collisions = False if self.task is None else self.robot.has_collisions(self.task, safety_margin=0) # TODO may need to alter safety margin
-        valid_pose = not (collisions or self_collision)
-        
-        if valid_pose:
-            end_effector_pose = tuple(round(coord, self.rounding) for coord in g[:3, 3].flatten())
-            return end_effector_pose
-        else:
-            return None
-        
     
-    def find_all_valid_poses(self):
-        """
-        Finds all the valid poses that are reachable by iterating through the robot's joint space.
+    def reachability_random_sample(self, num_samples):
+        """Reachable points with their manipulability index."""
+        reachable_points = set()
 
-        Args:
-            robot (timor.Robot.PinRobot): our robot
-            angle_interval (int): how many times do we split the range of angles from 0 to 2pi
-            world_res (float): used to round the valid poses (argument should look like 0.01m)
-            task (timor.task.Task, optional): task containing obstacles. Defaults to None.
-
-        Returns:
-            list[tuple[float, float, float]]: a list containing all valid poses x, y, z in the world space
-        """
-        # Generate all possible combinations of joint angles
-        num_joints = len(self.robot.joints)
-        angles = np.linspace(0, 2 * np.pi, self.angle_interval)
-        all_angles_combo = itertools.product(angles, repeat=num_joints)
-        
-        print(f"Angles = {angles}")
-        
-        # To store all valid poses, multiple joint combos may result in the same end-effector position
-        valid_poses = []
+        for _ in tqdm(range(num_samples)):
+            q = self.robot.random_configuration()
+            tcp_pose = self.robot.fk(q)
             
-        for theta in tqdm(list(all_angles_combo), desc="Finding Reachable Workspace"):
-            end_effector_pose = self.check_pose_and_get_gripper(theta)
-            if (end_effector_pose is not None):
-                valid_poses.append(end_effector_pose)
-        
-        self.valid_poses = valid_poses
-        return list(valid_poses)
-        
-        
-    def find_all_valid_poses_multiprocessor_pool(self):
-        """Finds all the valid poses (end effector position) that are reachable by 
-           iterating through the joint space
-
-        Returns:
-            list[tuple[float, float, float]]: a list containing all valid poses (x, y, z) in the world space
-        
-        Note: 
-            this does not work because you don't different processes do not have access to shared variables
-        """
-        # Generate all possible combinations of joint angles
-        num_joints = len(self.robot.joints)
-        angles = np.linspace(0, 2 * np.pi, self.angle_interval)
-        all_angles_combo = product(angles, repeat=num_joints)
-        
-        valid_poses = []
-        
-        with Pool() as p: 
-            valid_poses = p.map(self.check_pose_and_get_gripper, all_angles_combo)
+            # Check if end effector is fully pointing downwards.
+            # The 3rd column of the rotation matrix corresponds to the z-axis of the EE w.r.t. world frame
+            end_effector_z_axis = tcp_pose[:3, 2]
+            if all(end_effector_z_axis == self.ee_downwards_axis):
+                continue
             
-        return list(valid_poses)
-        
-    
-    def find_all_valid_poses_multithreading(self, num_threads):
-        """Finds all the valid poses (end effector position) that are reachable by
-           iterating through the joint space (FK)
-           
-        Returns:
-            list[tuple[float, float, float]]: a list containing all valid poses (x, y, z) in the world space
-            
-        if this doesn't work istg
-        """
-        print("hi")
-        num_joints = len(self.robot.joints)
-        angles = np.linspace(0, 2 * np.pi, self.angle_interval)
-        all_angles_combo = list(product(angles, repeat=num_joints))
-        thetas_work = np.array_split(all_angles_combo, num_threads)
-        
-        threads = []
-        all_valid_poses = []
-        
-        print("hello")
-        
-        def simplified_fk(robot, thetas):
-            T = np.eye(4)
-            for joint, theta in zip(robot.joints, thetas):
-                R = joint.get_rotation_matrix(theta)
-                p = joint.translation
-                T_joint = np.block([[R, p.reshape(3, 1)],
-                                    [np.zeros(3), 1]])
-                T = T @ T_joint
-            return T[:3, 3]  # Return only the position vector
-        
-        def thread_function(thetas_group):
-            valid_poses = []
-            
-            for t in tqdm(thetas_group):
-                g = simplified_fk(self.robot.joints, t)
-                # self_collision = self.robot.has_self_collision()
-                self_collision = False
-                collisions = False if self.task is None else self.robot.has_collisions(self.task, safety_margin=0) # TODO may need to alter safety margin
-                valid_pose = not (collisions or self_collision)
-        
-                if valid_pose:
-                    end_effector_pose = tuple(round(coord, self.rounding) for coord in g[:3, 3].flatten())
-                    valid_poses.append(end_effector_pose)
-            
-            # Only one thread writes to the all_valid_poses at a time
-            with lock:
-                all_valid_poses.extend(valid_poses)
+            # Check onfig has no collision
+            if self.robot.has_self_collision(q):
+                continue
                 
-        lock = threading.Lock()
-        
-        print("hola")
-        
-        for t in range(num_threads):
-            print(f"{t} thread created")
-            thread = threading.Thread(target=thread_function, args=(thetas_work[t],))
-            threads.append(thread)
-            thread.start()
+            # check collision between obstacles and current config (changed in has_self_collision call) 
+            if self.task and self.robot.has_collisions(task, safety_margin = self.safety_margin):
+                continue
+                
+            end_effector_pose = tuple(round(coord, 2) for coord in tcp_pose[:3, 3].flatten())
+
+            # Compute the Yoshikawa manipulability index
+            # High Yoshikawa manipulability index values indicate that the robot has good dexterity 
+            # and can move in multiple directions from the corresponding configurations. 
+            # Low manipulability index values indicate that corresponding configurations are less 
+            # dexterous and more susceptible to singularities.
+            manipulability_idk = self.robot.manipulability_index(q)
+            reachable_points.add((end_effector_pose, manipulability_idk))  
             
-        print("Waiting for threads")
-            
-        # Wait for all threads to complete
-        for thread in threads:
-            thread.join()
-            
-        self.valid_poses = all_valid_poses
-        return all_valid_poses
+        self.reachable, self.manipulability =  zip([reachable_points])
         
+
         
     def plot_reachability(self, reachable = None, filename="reachability_plot.png"):
         # Create a 3D plot
@@ -217,6 +106,7 @@ class Reachability():
         plt.close(fig) 
 
         print(f"Reachability plot saved to {filename}")
+        
         
     def plot_interactive_reachability(self, reachable = None):
         # Convert lists to numpy arrays for easier plotting
@@ -251,40 +141,6 @@ class Reachability():
         )
 
         fig.show()
-        
-    def reachability_random_sample(self, num_samples, task = None):
-        """Reachable points with their manipulability index."""
-        reachable_points = set()
-
-        for _ in tqdm(range(num_samples)):
-            q = self.robot.random_configuration()
-            tcp_pose = self.robot.fk(q)
-            
-            # Check if end effector is fully pointing downwards.
-            # The 3rd column of the rotation matrix corresponds to the z-axis of the EE w.r.t. world frame
-            end_effector_z_axis = tcp_pose[:3, 2]
-            if all(end_effector_z_axis == self.ee_downwards_axis):
-                continue
-            
-            # Check onfig has no collision
-            if self.robot.has_self_collision(q):
-                continue
-                
-            # check collision between obstacles and current config (changed in has_self_collision call) 
-            if task and self.robot.has_collisions(task, safety_margin = self.safety_margin):
-                continue
-                
-            end_effector_pose = tuple(round(coord, 2) for coord in tcp_pose[:3, 3].flatten())
-
-            # Compute the Yoshikawa manipulability index
-            # High Yoshikawa manipulability index values indicate that the robot has good dexterity 
-            # and can move in multiple directions from the corresponding configurations. 
-            # Low manipulability index values indicate that corresponding configurations are less 
-            # dexterous and more susceptible to singularities.
-            manipulability_idk = self.robot.manipulability_index(q)
-            reachable_points.add((end_effector_pose, manipulability_idk))  
-                
-        return list(reachable_points)
     
     
     def plot_interactive_reachability_with_manipulability(self, reachable, manipulability):
@@ -407,14 +263,27 @@ if __name__ == "__main__":
     # 6 AXIS ROBOT
     world_dimension = [3.20, 3.20, 3.20]
     default_robot = prebuilt_robots.get_six_axis_modrob()
+    
+    # Create a new task
+    task_header = TaskHeader(
+        ID='Dummy Cube',
+        tags=['test'],
+        author=['AZJ']
+    )
+    
+    # Add obstacles (as collision objects)
+    cube = Obstacle(ID = 'cube', collision = Box(parameters = dict(x = 0.2, y = 0.2, z = 0.2), pose = Transformation.from_translation([0.5, 0.5, 0.5])))
+    task = Task(task_header, obstacles=[cube])
+    task.visualize()
+    
     reachability = Reachability(robot = default_robot, 
                                 angle_interval = how_many_times_to_split_angle_range, 
                                 world_resolution = world_resolution, 
-                                world_dimension = world_dimension)
+                                world_dimension = world_dimension,
+                                task = task)
     
     start_t = time.time()
     valid_poses = reachability.reachability_random_sample(num_samples = 100000)
-    # valid_poses = reachability.find_all_valid_poses_multithreading(num_threads)
     print(f"Time to find reachability: {time.time() - start_t} seconds")
     
     reachable, manipulability =  zip(*valid_poses)
